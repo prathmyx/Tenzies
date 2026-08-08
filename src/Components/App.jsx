87feceeb64c50d12f31448d08d9f28629206280f
prathmyx <pratham.yadav.gr8@outlook.com>
2026-08-08 23:18:44 +0530

    feat:added dies

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,20 @@
-import { createRoot } from "react-dom/client";
+import Die from "./Die";
 
 export default function App() {
     return (
         <main>
-            
+            <div className="dies-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+            </div>
         </main>
     );
 }
