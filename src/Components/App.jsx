beaa207e7ac132261b5ae7daf719fd6f3d39a2f3
prathmyx <pratham.yadav.gr8@outlook.com>
2026-08-09 12:42:42 +0530

    feat:changed to die obj

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,34 +2,39 @@ import { useState } from "react";
 import Die from "./Die";
 
 export default function App() {
-    const [diesElement, setDiesElement] = useState(null);
+    const [dies, setDies] = useState([]);
     const [count, setCount] = useState(-1);
 
     const getAllDies = function() { 
         return new Array(10)
         .fill(0)
         .map((_, index) => 
-            <Die key={index} ind={index} value={Math.ceil(Math.random() * 6)} isHeld={false} handle={handleClick}/>
+            ({
+            key:index, 
+            ind:index, 
+            value:Math.ceil(Math.random() * 6),
+            isHeld:false
+        })
         );
     }
 
     function handleClick(count, index) {
         if (count === -1) {
-            setDiesElement(() => getAllDies());
+            setDies(() => getAllDies());
             setCount(prev => prev + 1);
         }
         else if (index === -1) {
-            setDiesElement((prevDies) => prevDies.map((die, index) => {
-                if (die.props.isHeld === false) {
-                    return <Die key={index} ind={index} value={Math.ceil(Math.random() * 6)} isHeld={false} handle={handleClick}/>;
+            setDies((prevDies) => prevDies.map((die, index) => {
+                if (die.isHeld === false) {
+                    return {...die, value: Math.ceil(Math.random() * 6)}
                 }
                 return die;
             }));
             setCount(prev => prev + 1);
         } else {
-            setDiesElement((prevDies) => prevDies.map((die, ind) => {
+            setDies((prevDies) => prevDies.map((die, ind) => {
                 if (ind === index) {
-                    return <Die key={ind} ind={ind} value={die.props.value} isHeld={!die.props.isHeld} handle={handleClick}/>;
+                    return {...die, isHeld: !die.isHeld}
                 } else {
                     return die;
                 }
@@ -42,7 +47,15 @@ export default function App() {
         <>
             <main>
                 <div className="dies-container">
-                    {diesElement}
+                    {dies.map((die, index) =>
+                        <Die 
+                        key={index} 
+                        ind={index} 
+                        value={die.value}
+                        isHeld={die.isHeld}
+                        handle={handleClick}
+                        />
+                    )}
                 </div>
 
                 <button className="roll-dice" onClick={() => handleClick(count, -1)}>{count === -1 ? "New Game" : "Roll"}</button>
