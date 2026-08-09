af46d8aa65d0b3cdeb47580d15655c0b3e996d8a
prathmyx <pratham.yadav.gr8@outlook.com>
2026-08-09 14:22:37 +0530

    feat:styled p

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
             <main>
                 {win && <Confetti />}
                 <div>
-                    {win && <p>Congratulations! You won in {count} steps! Press "New Game" to start again.</p>}
+                    {win && <p>Congratulations! You won in {count} steps! <br/> Press <b>"New Game"</b> to start again.</p>}
                 </div>
                 <h1 className="title">Tenzies</h1>
                 {!win ? <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p> : "" }
